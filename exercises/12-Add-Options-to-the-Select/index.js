49002606afede068047dfdfaa08f0d4f07dc0449
Angelina <angelaaquin13@gmail.com>
2024-08-23 02:48:11 +0000

    initial

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,3 +1,16 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
 // Your code here
+for(let i=0; i<countries.length;i++){
+    const para = document.createElement("option");
+    para.innerHTML = countries[i];
+    document.getElementById("mySelect").appendChild(para);
+}
+
+function displaySelection(){
+    let e = document.getElementById("mySelect");
+   
+    alert(`${ e.options[e.selectedIndex].text}`);
+}
+
+document.getElementById("mySelect").addEventListener("change", displaySelection);
